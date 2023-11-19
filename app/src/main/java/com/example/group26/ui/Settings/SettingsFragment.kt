979a7ff8c8601019385f4251ad323ee0964f129e
carson <carson_lin@sfu.ca>
2023-11-18 18:27:00 -0800

    Added settings page, about us category not implemented. Also needs more categories to fill out page.
    Changed icon resources.

--- app/src/main/java/com/example/group26/ui/Settings/SettingsFragment.kt
+++ app/src/main/java/com/example/group26/ui/Settings/SettingsFragment.kt
@@ -1,34 +1,12 @@
 package com.example.group26.ui.Settings
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.group26.databinding.FragmentSettingsBinding
+import androidx.preference.PreferenceFragmentCompat
+import com.example.group26.R
 
-class SettingsFragment : Fragment() {
 
-    private var _binding: FragmentSettingsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val settingsViewModel =
-            ViewModelProvider(this).get(SettingsViewModel::class.java)
-        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.preferences, rootKey)
     }
 }
