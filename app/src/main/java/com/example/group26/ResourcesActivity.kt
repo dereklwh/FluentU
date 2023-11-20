8737e4539865c4b46bca1f5efcc8aa5a438b8f63
Derek <derek141772@gmail.com>
2023-11-20 11:53:23 -0800

    resource button

--- app/src/main/java/com/example/group26/ResourcesActivity.kt
+++ app/src/main/java/com/example/group26/ResourcesActivity.kt
@@ -1,12 +1,19 @@
 package com.example.group26
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ResourcesActivity:AppCompatActivity() {
+
+    private lateinit var resourceButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_resources)
+
+        resourceButton = findViewById(R.id.resource_Button)
+        resourceButton.setOnClickListener{
+            finish()
+        }
     }
 }
