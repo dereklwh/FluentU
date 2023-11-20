a6d7a4342c86f74614152e986724cabd1675e410
carson <carson_lin@sfu.ca>
2023-11-20 11:53:52 -0800

    adding close button for progress activity

--- app/src/main/java/com/example/group26/ProgressActivity.kt
+++ app/src/main/java/com/example/group26/ProgressActivity.kt
@@ -1,6 +1,7 @@
 package com.example.group26
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class ProgressActivity: AppCompatActivity() {
@@ -8,5 +9,11 @@ class ProgressActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_progress)
+
+        val close: Button = findViewById(R.id.progressCloseButton)
+
+        close.setOnClickListener(){
+            finish()
+        }
     }
 }
