372608aa118c1703e20ca9d14c981d99f4f3511d
student186 <saintdude111@gmail.com>
2023-11-22 00:48:56 -0800

    profile fields now savable except for the photo

--- app/src/main/java/com/example/group26/ui/Profile/ProfileFragment.kt
+++ app/src/main/java/com/example/group26/ui/Profile/ProfileFragment.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.group26.R
 import com.example.group26.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
-
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
 
@@ -21,9 +22,48 @@ class ProfileFragment : Fragment() {
         val profileViewModel =
             ViewModelProvider(this).get(ProfileViewModel::class.java)
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+	    val closeButton = binding.closeButton
+	    closeButton.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+
+        val saveButton = binding.saveButton
+        saveButton.setOnClickListener {
+            saveProfileData(profileViewModel)
+        }
+
+        val savedProfileData = profileViewModel.getSavedProfileData()
+        binding.editName.setText(savedProfileData.first)
+        binding.editEmail.setText(savedProfileData.second)
+        binding.editPhone.setText(savedProfileData.third)
+
+        val savedGender = profileViewModel.getSavedGender()
+        if (savedGender == "Male") {
+            binding.maleButton.isChecked = true
+        }
+        else if (savedGender == "Female") {
+            binding.femaleButton.isChecked = true
+        }
+
         return binding.root
     }
 
+    private fun saveProfileData(profileViewModel: ProfileViewModel) {
+        val name = binding.editName.text.toString()
+        val email = binding.editEmail.text.toString()
+        val phone = binding.editPhone.text.toString()
+
+        val radioGroup = binding.radioGroup
+        val selectedGenderId = radioGroup.checkedRadioButtonId
+        val gender = when (selectedGenderId) {
+            R.id.maleButton -> "Male"
+            R.id.femaleButton -> "Female"
+            else -> ""
+        }
+        profileViewModel.saveProfileData(name, email, phone, gender)
+        Toast.makeText(requireContext(), "Profile saved", Toast.LENGTH_SHORT).show()
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
--- app/src/main/java/com/example/group26/ui/Profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/group26/ui/Profile/ProfileViewModel.kt
@@ -1,9 +1,33 @@
 package com.example.group26.ui.Profile
 
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class ProfileViewModel : ViewModel() {
+class ProfileViewModel(application: Application) : AndroidViewModel(application) {
+    private val sharedPreferences = application.getSharedPreferences("ProfilePreferences", Context.MODE_PRIVATE)
 
+    fun saveProfileData(name: String, email: String, phone: String, gender: String) {
+        sharedPreferences.edit().apply {
+            putString("NAME", name)
+            putString("EMAIL", email)
+            putString("PHONE", phone)
+            putString("GENDER", gender)
+            apply()
+        }
+    }
+
+    fun getSavedProfileData(): Triple<String, String, String> {
+        val name = sharedPreferences.getString("NAME", "") ?: ""
+        val email = sharedPreferences.getString("EMAIL", "") ?: ""
+        val phone = sharedPreferences.getString("PHONE", "") ?: ""
+        return Triple(name, email, phone)
+    }
+
+    fun getSavedGender(): String {
+        return sharedPreferences.getString("GENDER", "") ?: ""
+    }
 }
