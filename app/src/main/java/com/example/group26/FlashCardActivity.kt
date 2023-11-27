23d5717f806b84197736e043584d1be28ba36f39
carson <carson_lin@sfu.ca>
2023-11-27 01:16:30 -0800

    fixed preferences values and finished flashCardActivity

--- app/src/main/java/com/example/group26/FlashCardActivity.kt
+++ app/src/main/java/com/example/group26/FlashCardActivity.kt
@@ -1,69 +1,83 @@
 package com.example.group26
 
+import android.content.SharedPreferences
 import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
-import android.util.Log
 import android.view.Gravity
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.preference.PreferenceManager
 import com.example.group26.database.FlashcardEntry
 import com.example.group26.viewmodels.FlashCardViewModel
 import com.example.group26.viewmodels.FlashCardViewModelFactory
-import com.example.group26.viewmodels.QuizViewModel
-import com.example.group26.viewmodels.QuizViewModelFactory
-import com.google.cloud.translate.Translate
-import com.google.cloud.translate.TranslateOptions
-import com.google.cloud.translate.Translation
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+
 
 class FlashCardActivity: AppCompatActivity() {
     private val api: String = "AIzaSyC0LA82UScnqYhuh-e_urF_aH7h_CZ-y7A"
     private lateinit var viewModel: FlashCardViewModel
+    private lateinit var translator:Translator
+    private lateinit var language:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flashcards)
         val factory = FlashCardViewModelFactory(this.application)
         viewModel = ViewModelProvider(this, factory).get(FlashCardViewModel::class.java)
-
+        translator = Translator(api)
+        val sharedPreferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        language = sharedPreferences.getString("language_preference", "en") ?: "en"
         setupAddFlashCardButton()
         setupObservers()
+        loadFlashCards()
     }
 
+    private fun loadFlashCards()
+    {
+        val flashcardsContainer: LinearLayout = findViewById(R.id.flashcardsContainer)
+        flashcardsContainer.removeAllViews()
+        val flashcards: List<FlashcardEntry>? = viewModel.allFlashCards.value
+        flashcards?.let { list ->
+            for (flashcard in list) {
+                addFlashcard(flashcard)
+            }
+        }
+    }
     private fun setupAddFlashCardButton(){
         val button:Button = findViewById(R.id.addFlashcardButton)
         button.setOnClickListener(){
             val editText:EditText = findViewById(R.id.flashCardEditText)
             val inputString = editText.text.toString()
             if(inputString != ""){
-                val entry = FlashcardEntry(
-                    englishPhrase = inputString,
-                    frenchPhrase = translateText(inputString, "fr"),
-                    spanishPhrase = translateText(inputString, "es"),
-                    chinesePhrase = translateText(inputString, "zh")
-                )
-                viewModel.insertFlashCard(entry)
+                CoroutineScope(Dispatchers.Main).launch {
+                        val entry = FlashcardEntry(
+                            englishPhrase = inputString,
+                            frenchPhrase = translator.translateText(inputString, "fr"),
+                            spanishPhrase = translator.translateText(inputString, "es"),
+                            chinesePhrase = translator.translateText(inputString, "zh-CN")
+                        )
+                    viewModel.insertFlashCard(entry)
+                }
             }
         }
     }
 
     private fun setupObservers(){
-        viewModel.allFlashCards.observe(this, Observer{flashcards ->
+        viewModel.allFlashCards.observe(this) { flashcards ->
             val flashcard = flashcards.getOrNull(flashcards.size - 1)
             if (flashcard != null) {
-                addFlashcard(flashcard.englishPhrase, flashcard.id)
+                loadFlashCards()
             }
-        })
+        }
     }
-    private fun addFlashcard(word:String, id:Long) {
+
+    private fun addFlashcard(word:FlashcardEntry) {
         val flashcardsContainer: LinearLayout = findViewById(R.id.flashcardsContainer)
 
         val flashCard = LinearLayout(this)
@@ -82,6 +96,7 @@ class FlashCardActivity: AppCompatActivity() {
         deleteButton.setBackgroundColor(Color.TRANSPARENT)
         deleteButton.setOnClickListener {
             flashcardsContainer.removeView(flashCard)
+            viewModel.deleteFlashCard(word)
         }
         val deleteParams = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.WRAP_CONTENT,
@@ -93,7 +108,7 @@ class FlashCardActivity: AppCompatActivity() {
 
         val flashCardWord = TextView(this)
         flashCardWord.setTypeface(null, Typeface.BOLD)
-        flashCardWord.text = word
+        flashCardWord.text = word.englishPhrase
         flashCardWord.gravity = Gravity.CENTER
         flashCard.addView(flashCardWord)
 
@@ -102,7 +117,6 @@ class FlashCardActivity: AppCompatActivity() {
         flashCardSubText.gravity = Gravity.CENTER
         flashCard.addView(flashCardSubText)
 
-
         flashcardsContainer.addView(flashCard)
 
         //animation from chatgpt
@@ -110,19 +124,30 @@ class FlashCardActivity: AppCompatActivity() {
             flashCard.animate().scaleX(0.95f).scaleY(0.95f).setDuration(100).withEndAction {
                 flashCard.animate().scaleX(1f).scaleY(1f).setDuration(100).start()
             }
-
-        }
-    }
-
-
-    private fun translateText(text: String, targetLanguage: String): String {
-        return try {
-            val translate: Translate = TranslateOptions.newBuilder().setApiKey(api).build().service
-            val translation: Translation = translate.translate(text, Translate.TranslateOption.targetLanguage(targetLanguage))
-            translation.translatedText
-        } catch (e: Exception) {
-            e.printStackTrace()
-            "Error: ${e.localizedMessage}"
+            if(language == "fr"){
+                if(flashCardWord.text == word.frenchPhrase){
+                    flashCardWord.text = word.englishPhrase
+                }
+                else{
+                    flashCardWord.text = word.frenchPhrase
+                }
+            }
+            else if(language == "es"){
+                if(flashCardWord.text == word.spanishPhrase){
+                    flashCardWord.text = word.englishPhrase
+                }
+                else{
+                    flashCardWord.text = word.spanishPhrase
+                }
+            }
+            else if(language == "zh-CN"){
+                if(flashCardWord.text == word.chinesePhrase){
+                    flashCardWord.text = word.englishPhrase
+                }
+                else{
+                    flashCardWord.text = word.chinesePhrase
+                }
+            }
         }
     }
 }
