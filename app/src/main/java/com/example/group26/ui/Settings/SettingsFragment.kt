26ffe33870aad5a371865d7d014c415d8bf61f71
Derek <derek141772@gmail.com>
2023-12-04 12:54:09 -0800

    website to settings fragment

--- app/src/main/java/com/example/group26/ui/Settings/SettingsFragment.kt
+++ app/src/main/java/com/example/group26/ui/Settings/SettingsFragment.kt
@@ -2,14 +2,22 @@ package com.example.group26.ui.Settings
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.example.group26.R
+import android.net.Uri;
 
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+
+        findPreference<Preference>("about_us_preference")?.setOnPreferenceClickListener {
+            val url = "https://derek141772.wixsite.com/fluentu"
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+            true
+        }
     }
-
-
 }
